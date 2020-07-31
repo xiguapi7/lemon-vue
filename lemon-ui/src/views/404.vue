e7a5c2c2b0ca3fa4b4181273c8a71c8190af8e82
大大大西西瓜皮 <adanz1015@outlook.com>
2020-08-01 03:41:58 +0800

    项目搭建.

---
+++ lemon-ui/src/views/404.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="page">
+    <h2>404</h2>
+  </div>
+</template>
+
+<script>
+export default {
+name: "404"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ lemon-ui/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="page">
+    <h2>Home page</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
